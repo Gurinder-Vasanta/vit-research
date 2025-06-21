3bef017c4575464e39fbfdd4b38cf38d71ebfe73
Gurinder Vasanta <gcvasanta04@gmail.com>
2025-06-21 10:41:04 -0400

    added new video

--- nba_proj/finding_ball.py
+++ nba_proj/finding_ball.py
@@ -5,11 +5,13 @@ import subprocess
 
 # purpose of this one is to teach the model when the ball is somewhere near the rim
 def download_video(url, output_path):
-    command = ["yt-dlp", "-f", "616", "-o", output_path, url]
+    command = ["yt-dlp", "-f", "136", "-o", output_path, url]
     subprocess.run(command, check=True)
     print(f"Video downloaded to {output_path}")
 
-vid_url = 'https://www.youtube.com/watch?v=SdvAG7dgrT0'
-output_path = 'hoop_ball_finding.mp4'
+vid_url = 'https://www.youtube.com/watch?v=I33o9UnUe1A&list=PLlVlyGVtvuVniS7jx4DyESaKRUANIzyhE'
+
+# okc game: https://www.youtube.com/watch?v=I33o9UnUe1A&list=PLlVlyGVtvuVniS7jx4DyESaKRUANIzyhE
+output_path = 'data/raw_videos/okc_2016.mp4'
 
 download_video(vid_url,output_path)
