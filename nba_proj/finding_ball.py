cfc899af845161c94db382493bd072130a8383ba
Gurinder Vasanta <gcvasanta04@gmail.com>
2025-06-23 19:04:49 -0400

    added another vid

--- nba_proj/finding_ball.py
+++ nba_proj/finding_ball.py
@@ -9,9 +9,10 @@ def download_video(url, output_path):
     subprocess.run(command, check=True)
     print(f"Video downloaded to {output_path}")
 
-vid_url = 'https://www.youtube.com/watch?v=I33o9UnUe1A&list=PLlVlyGVtvuVniS7jx4DyESaKRUANIzyhE'
+vid_url = 'https://www.youtube.com/watch?v=H56iR3188P0'
 
-# okc game: https://www.youtube.com/watch?v=I33o9UnUe1A&list=PLlVlyGVtvuVniS7jx4DyESaKRUANIzyhE
-output_path = 'data/raw_videos/okc_2016.mp4'
+# okc game: https://www.youtube.com/watch?v=I33o9UnUe1A&list=PLlVlyGVtvuVniS7jx4DyESaKRUANIzyhE (vid2)
+# heat game: https://www.youtube.com/watch?v=H56iR3188P0 (vid3; removed playlist stuff)
+output_path = 'data/raw_videos/heat_2014.mp4'
 
 download_video(vid_url,output_path)
