68bb5ba3386d660fd8fb283e6c44cb6b09b22f8f
Gurinder Vasanta <gcvasanta04@gmail.com>
2025-06-28 13:30:46 -0400

    will eventually split vids into clips

---
+++ nba_proj/generate_clips.py
@@ -0,0 +1,22 @@
+# temp = ['vid1_frame_11524.jpg', 
+#         'vid2_frame_9477.jpg', 
+#         'vid1_frame_24051.jpg', 
+#         'vid1_frame_14255.jpg', 
+#         'vid1_frame_12844.jpg', 
+#         'vid2_frame_7377.jpg', 
+#         'vid2_frame_11734.jpg', 
+#         'vid1_frame_24527.jpg', 
+#         'vid1_frame_21875.jpg', 
+#         'vid1_frame_10371.jpg', 
+#         'vid1_frame_22800.jpg', 
+#         'vid2_frame_9606.jpg', 
+#         'vid1_frame_13513.jpg', 
+#         'vid1_frame_23472.jpg']
+# def comparator(fname):
+#     splitted = fname.split('_')
+#     vid_num = int(splitted[0][3::])
+#     frame_num = int(splitted[2].split('.')[0])
+#     return (vid_num, frame_num)
+
+# print(sorted(temp,key = comparator))
+
