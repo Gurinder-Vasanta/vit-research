09a49dd29d756191f2839cf25d195fb349a22bd0
Gurinder Vasanta <gcvasanta04@gmail.com>
2026-01-03 15:10:17 -0500

    added chunk size and print every

--- nba_proj/config.py
+++ nba_proj/config.py
@@ -1,9 +1,9 @@
-RAG_WEIGHTS = "rag_weights/rag_head_6_vids.weights.h5"
-PROJ_WEIGHTS = "proj_weights/projection_head_6_vids.weights.h5"
+RAG_WEIGHTS = "rag_weights/rag_head_test_vids_bs2.weights.h5"
+PROJ_WEIGHTS = "proj_weights/projection_head_test_vids_bs2.weights.h5"
 
-CHROMADB_COLLECTION = "rich_embeddings_6vids"
+CHROMADB_COLLECTION = "rich_embeddings_test_vids_bs2"
 
-TOP_K = 100
+TOP_K = 10
 SEARCH_K = 750
 
 EPOCHS = 24
@@ -17,15 +17,19 @@ PHASE_1_LEARNING_RATE = 1e-4
 PHASE_2_LEARNING_RATE = 1e-6
 
 NUM_QUERIES = 16
-NUM_LAYERS = 4
+NUM_LAYERS = 6
 NUM_HEADS = 8
 
 START_CHUNK_TRAIN = 0
-END_CHUNK_TRAIN = 2500
+END_CHUNK_TRAIN = 100
 
-START_CHUNK_VALID = 3000
-END_CHUNK_VALID = 3140
+START_CHUNK_VALID = 120
+END_CHUNK_VALID = 130
 
-NUM_CLIPS_PER_VID = 15
-VIDS_TO_USE = ["vid2", 'vid3','vid4','vid6','vid8','vid10']
+CHUNK_BATCH_SIZE = 6
+PRINT_EVERY = 5
+
+NUM_CLIPS_PER_VID = 2
+VIDS_TO_USE = ['vid3','vid4','vid6']
+# VIDS_TO_USE = ['vid1',"vid2", 'vid3','vid4','vid6','vid8','vid10']
 # VIDS_TO_USE = ['vid3']
