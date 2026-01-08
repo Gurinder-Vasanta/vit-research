54535a3a0a718b148d506572b030774164b08fdb
Gurinder Vasanta <gcvasanta04@gmail.com>
2026-01-08 13:07:07 -0500

    added few more config params

--- nba_proj/config.py
+++ nba_proj/config.py
@@ -1,35 +1,38 @@
-RAG_WEIGHTS = "rag_weights/rag_head_test_vids_bs2.weights.h5"
-PROJ_WEIGHTS = "proj_weights/projection_head_test_vids_bs2.weights.h5"
+# rag_head_7_vids_bs4_cls_only_12_epochs
+RAG_WEIGHTS = "rag_weights/rag_head_7_vids_bs4_cls_rag_12_epochs.weights.h5"
+PROJ_WEIGHTS = 'proj_weights/projection_7_vids_bs4_cls_rag_12_epochs.weights.h5'
 
-CHROMADB_COLLECTION = "rich_embeddings_test_vids_bs2"
+CHROMADB_COLLECTION = "rich_embeddings_7_vids_bs4_cls_rag_12_epochs"
 
-TOP_K = 10
+TOP_K = 100
 SEARCH_K = 750
 
-EPOCHS = 24
+EPOCHS = 12
 REBUILD_EVERY = 4
 ACCUM_BATCH_SIZE = 8
 
-PHASE_1_CONTRASTIVE_LOSS = 0.0
-PHASE_2_CONTRASTIVE_LOSS = 0.1
+ADJUST_CONTRASTIVE_LOSS_EVERY = 2
+INCREMENT_CONTRASTIVE_LOSS_BY = 0.05
+# PHASE_1_CONTRASTIVE_LOSS = 0.0
+# PHASE_2_CONTRASTIVE_LOSS = 0.1
 
-PHASE_1_LEARNING_RATE = 1e-4
+PHASE_1_LEARNING_RATE = 1e-5
 PHASE_2_LEARNING_RATE = 1e-6
 
 NUM_QUERIES = 16
-NUM_LAYERS = 6
+NUM_LAYERS = 4
 NUM_HEADS = 8
 
 START_CHUNK_TRAIN = 0
-END_CHUNK_TRAIN = 100
+END_CHUNK_TRAIN = 3000
 
-START_CHUNK_VALID = 120
-END_CHUNK_VALID = 130
+START_CHUNK_VALID = 3100
+END_CHUNK_VALID = 3200
 
-CHUNK_BATCH_SIZE = 6
-PRINT_EVERY = 5
+CHUNK_BATCH_SIZE = 4
+PRINT_EVERY = 25
 
-NUM_CLIPS_PER_VID = 2
-VIDS_TO_USE = ['vid3','vid4','vid6']
-# VIDS_TO_USE = ['vid1',"vid2", 'vid3','vid4','vid6','vid8','vid10']
+NUM_CLIPS_PER_VID = 15
+# VIDS_TO_USE = ['vid3','vid4','vid6']
+VIDS_TO_USE = ['vid1',"vid2", 'vid3','vid4','vid6','vid8','vid10']
 # VIDS_TO_USE = ['vid3']
