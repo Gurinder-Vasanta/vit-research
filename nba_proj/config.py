143bc0f2571ff3a4173eb163d4965a7f34441c55
Gurinder Vasanta <gcvasanta04@gmail.com>
2026-01-13 13:56:01 -0500

    rag config

--- nba_proj/config.py
+++ nba_proj/config.py
@@ -1,18 +1,21 @@
+import os 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CHROMA_PATH = os.path.join(BASE_DIR, "chroma_store")
 # rag_head_7_vids_bs4_cls_only_12_epochs
-RAG_WEIGHTS = "rag_weights/rag_head_7_vids_bs4_cls_rag_12_epochs.weights.h5"
-PROJ_WEIGHTS = 'proj_weights/projection_7_vids_bs4_cls_rag_12_epochs.weights.h5'
+RAG_WEIGHTS = "rag_weights/rag_head_7_vids_bs4_cls_only_12_epochs_rebuild.weights.h5"
+PROJ_WEIGHTS = 'proj_weights/projection_7_vids_bs4_cls_only_12_epochs_rebuild.weights.h5'
 
-CHROMADB_COLLECTION = "rich_embeddings_7_vids_bs4_cls_rag_12_epochs"
+CHROMADB_COLLECTION = "rich_embeddings_7_vids_bs4_cls_only_12_epochs_rebuild"
 
 TOP_K = 100
 SEARCH_K = 750
 
 EPOCHS = 12
-REBUILD_EVERY = 4
+REBUILD_EVERY = 3
 ACCUM_BATCH_SIZE = 8
 
 ADJUST_CONTRASTIVE_LOSS_EVERY = 2
-INCREMENT_CONTRASTIVE_LOSS_BY = 0.05
+INCREMENT_CONTRASTIVE_LOSS_BY = 0.025
 # PHASE_1_CONTRASTIVE_LOSS = 0.0
 # PHASE_2_CONTRASTIVE_LOSS = 0.1
 
@@ -27,7 +30,7 @@ START_CHUNK_TRAIN = 0
 END_CHUNK_TRAIN = 3000
 
 START_CHUNK_VALID = 3100
-END_CHUNK_VALID = 3200
+END_CHUNK_VALID = 3300
 
 CHUNK_BATCH_SIZE = 4
 PRINT_EVERY = 25
